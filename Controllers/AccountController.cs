8c4e96102325851cd22d7b32e91f2cac6cc06c00
AirborneFluff <proffitt.m.98@gmail.com>
2023-09-26 20:25:45 +0100

    Updated missing change

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Azure_WebApi.Controllers;
+namespace RaffleApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
