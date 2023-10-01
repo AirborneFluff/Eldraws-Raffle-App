fdd559d9650448753f099fd8a30d26da41a6b175
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-01 22:01:37 +0100

    Begun building app frame

--- client/src/app/core/components/app-frame/app-frame.component.ts
+++ client/src/app/core/components/app-frame/app-frame.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../../services/account.service';
 
 @Component({
   selector: 'app-frame',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppFrameComponent {
 
+  constructor(public account: AccountService) {
+  }
 }
