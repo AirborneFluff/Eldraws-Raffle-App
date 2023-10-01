d9a7f5607cafabc1be7c604c4eb894c988b994e2
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-01 20:26:24 +0100

    Added auth interceptor

---
+++ client/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, take } from 'rxjs';
+import { AccountService } from '../services/account.service';
+import { AppUser } from '../../data/models/app-user';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private account: AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let currentUser!: AppUser | null;
+
+    this.account.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
+
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
