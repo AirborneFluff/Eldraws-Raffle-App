483c889924976dd0154e38fce84b2bc139e1b3d0
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-01 20:59:39 +0100

    Creating app frame

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { AppFrameComponent } from './components/app-frame/app-frame.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppFrameComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  exports: [
+    AppFrameComponent
   ]
 })
 export class CoreModule { }
