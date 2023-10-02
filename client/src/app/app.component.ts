d2ffa6ed343fdefc7c38d6205e8f3e58155fd57c
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-02 19:21:07 +0100

    Added theming service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { AccountService } from "./core/services/account.service";
+import { AccountService } from './core/services/account.service';
+import { ThemingService } from './core/theming/theming.service';
+import { Theme } from './core/theming/theme';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,7 @@ import { AccountService } from "./core/services/account.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public account: AccountService) {
+  constructor(public account: AccountService, private theme: ThemingService) {
+    theme.setTheme(Theme.Dark)
   }
 }
