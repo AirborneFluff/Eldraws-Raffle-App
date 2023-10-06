3e1de738fe1aeda44f454f0aecf806ad325ca838
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-06 23:19:07 +0100

    Added totals

--- client/src/app/features/raffles/raffle-details/raffle-details.component.ts
+++ client/src/app/features/raffles/raffle-details/raffle-details.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../../core/services/api.service';
 import {
-  combineLatest, of,
+  combineLatest, map, of,
   switchMap
 } from 'rxjs';
 import { RaffleIdStream } from '../../../core/streams/raffle-id-stream';
 import { ClanIdStream } from '../../../core/streams/clan-id-stream';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
 import { RaffleStream } from '../../../core/streams/raffle-stream';
+import { Raffle } from '../../../data/models/raffle';
 
 @Component({
   selector: 'app-raffle-details',
@@ -26,5 +27,16 @@ export class RaffleDetailsComponent {
       }))
 
   constructor(private api: ApiService, private raffleId$: RaffleIdStream, private clanId$: ClanIdStream, private raffleUpdates$: RaffleStream) {
+  }
+
+  getDonations(raffle: Raffle): number {
+    return raffle.entries.reduce((acc: number, curr) => acc + curr.donation, 0)
+  }
+
+  getTickets(raffle: Raffle): number {
+    return raffle.entries.reduce((max, entry) => {
+      const item2 = entry.tickets?.item2 ?? 0; // Use 0 as a default value if Item2 is missing
+      return item2 > max ? item2 : max;
+    }, -Infinity);
   }
 }
