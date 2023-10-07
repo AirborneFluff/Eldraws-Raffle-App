21e5e2a1b5cd4970398ce670fddd5a907c31a6e9
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-07 16:56:19 +0100

    Fixed navigation baseRoute issue

--- client/src/app/core/components/app-frame/app-frame.component.ts
+++ client/src/app/core/components/app-frame/app-frame.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../../services/account.service';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-frame',
@@ -12,6 +13,13 @@ export class AppFrameComponent {
 
   constructor(public account: AccountService, public title: Title, private route: ActivatedRoute, private router: Router) {
   }
+
+  baseRoute$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map(navEnd => {
+      const event: NavigationEnd = navEnd as NavigationEnd;
+      return event.url.split('/').length == 2;
+    }))
 
   back() {
     this.router.navigate(['../'], {relativeTo: this.route})
