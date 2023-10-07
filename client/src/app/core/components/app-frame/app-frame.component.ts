042addde50df63885f1b02d3992f3322e9429433
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-07 15:37:45 +0100

    Routes updated (#14)

--- client/src/app/core/components/app-frame/app-frame.component.ts
+++ client/src/app/core/components/app-frame/app-frame.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../../services/account.service';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-frame',
@@ -9,6 +10,10 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppFrameComponent {
 
-  constructor(public account: AccountService, public title: Title) {
+  constructor(public account: AccountService, public title: Title, private route: ActivatedRoute, private router: Router) {
+  }
+
+  back() {
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 }
