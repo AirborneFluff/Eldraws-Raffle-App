728517e7f83699a37bb2e5ccf5db99ef63a5a258
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-15 20:26:56 +0100

    Added status endpoint for uptime checks

---
+++ API/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaffleApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class StatusController : ControllerBase
+{
+    [HttpHead]
+    public async Task<ActionResult> GetResponse()
+    {
+        return Ok();
+    }
+}
