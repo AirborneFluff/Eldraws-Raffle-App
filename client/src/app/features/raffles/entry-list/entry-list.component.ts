c680df0119d27be99ddd01b93a4931f5010a2b99
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-15 22:09:36 +0100

    Added placeholders and loading templates (#56)

--- client/src/app/features/raffles/entry-list/entry-list.component.ts
+++ client/src/app/features/raffles/entry-list/entry-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { RaffleEntry } from '../../../data/models/raffle-entry';
 import { ApiService } from '../../../core/services/api.service';
 import { ClanIdStream } from '../../../core/streams/clan-id-stream';
 import { RaffleIdStream } from '../../../core/streams/raffle-id-stream';
-import { combineLatest, of, switchMap } from 'rxjs';
+import { combineLatest, of, Subscription, switchMap } from 'rxjs';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
 import { RaffleStream } from '../../../core/streams/raffle-stream';
 import { MatDialog } from '@angular/material/dialog';
@@ -14,14 +14,20 @@ import { ConfirmDialogComponent } from '../../../shared/dialog/confirm-dialog/co
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.scss']
 })
-export class EntryListComponent {
+export class EntryListComponent implements OnDestroy {
   @Input() entries: RaffleEntry[] = [];
+
+  subscription = new Subscription();
 
   constructor(private api: ApiService, private clanId$: ClanIdStream, private raffleId$: RaffleIdStream, private raffleUpdates$: RaffleStream, private dialog: MatDialog) {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   removeEntry(entry: RaffleEntry) {
-    this.dialog.open(ConfirmDialogComponent, {
+    const subscription = this.dialog.open(ConfirmDialogComponent, {
       data: {
         title: 'Are you sure?',
         message: `Removing this entry will re-assign all ticket assignments beyond the deleted entry <br>${entry.entrant.gamertag} : ${entry.tickets.item1} - ${entry.tickets.item2}`,
@@ -39,5 +45,7 @@ export class EntryListComponent {
     })).subscribe(value => {
       this.raffleUpdates$.next(value)
     })
+
+    this.subscription.add(subscription);
   }
 }
