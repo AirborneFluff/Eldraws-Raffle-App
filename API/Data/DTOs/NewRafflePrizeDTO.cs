ce241312833168c2abdcc6a60953de823d05ab02
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-27 20:55:01 +0100

    Allowed for dynamic prizes

--- API/Data/DTOs/NewRafflePrizeDTO.cs
+++ API/Data/DTOs/NewRafflePrizeDTO.cs
@@ -7,4 +7,6 @@ public class NewRafflePrizeDTO
     public int Place { get; set; }
     [Required]
     public string Description { get; set; }
+    [Range(0f, 1f)]
+    public float DonationPercentage { get; set; }
 }
--- API/Data/DTOs/RafflePrizeInfoDTO.cs
+++ API/Data/DTOs/RafflePrizeInfoDTO.cs
@@ -3,6 +3,7 @@
 public class RafflePrizeInfoDTO
 {
     public int Place { get; set; }
+    public float DonationPercentage { get; set; }
     public string? Description { get; set; }
     public int? WinningTicketNumber { get; set; }
 }
