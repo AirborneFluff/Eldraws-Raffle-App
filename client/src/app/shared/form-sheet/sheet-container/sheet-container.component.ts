c1c1360996c16c476868d6caa04a5a1a8b96777a
AirborneFluff <proffitt.m.98@gmail.com>
2023-10-28 12:03:00 +0100

    Added generic sheet container component

---
+++ client/src/app/shared/form-sheet/sheet-container/sheet-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sheet-container',
+  templateUrl: './sheet-container.component.html',
+  styleUrls: ['./sheet-container.component.scss']
+})
+export class SheetContainerComponent {
+
+}
