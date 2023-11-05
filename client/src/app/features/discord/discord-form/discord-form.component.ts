53f59b9881d0385abd54af3a1e567a86fa463c16
AirborneFluff <proffitt.m.98@gmail.com>
2023-11-05 16:57:00 +0000

    Added error handling

--- client/src/app/features/discord/discord-form/discord-form.component.ts
+++ client/src/app/features/discord/discord-form/discord-form.component.ts
@@ -3,7 +3,7 @@ import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ApiService } from '../../../core/services/api.service';
 import { ClanIdStream } from '../../../core/streams/clan-id-stream';
 import { RaffleIdStream } from '../../../core/streams/raffle-id-stream';
-import { combineLatest, map, switchMap, take } from 'rxjs';
+import { BehaviorSubject, combineLatest, finalize, map, switchMap, take, tap } from 'rxjs';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
 import { CurrentClanStream } from '../../../core/streams/current-clan-stream';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -26,6 +26,9 @@ export class DiscordFormComponent {
     notNullOrUndefined()
   )
 
+  postError$ = new BehaviorSubject<boolean>(false);
+  submitted$ = new BehaviorSubject<boolean>(false);
+
   constructor(public bottomSheet: MatBottomSheet, private api: ApiService, private clanId$: ClanIdStream, private raffleId$: RaffleIdStream, private clan$: CurrentClanStream) {
     this.clan$.pipe(
       notNullOrUndefined(),
@@ -40,16 +43,21 @@ export class DiscordFormComponent {
       this.clanId$.pipe(notNullOrUndefined()),
       this.raffleId$.pipe(notNullOrUndefined())
     ]).pipe(
+      tap(() => {
+        this.postError$.next(false);
+        this.submitted$.next(true);
+      }),
       take(1),
       switchMap(([clanId, raffleId]) => {
         return this.api.Raffles.createDiscordPost(clanId, raffleId);
-      })
+      }),
+      finalize(() => this.submitted$.next(false))
     ).subscribe({
       next: () => {
         this.bottomSheet.dismiss();
       },
       error: e => {
-        console.log(e)
+        this.postError$.next(true);
       }
     })
   }
