f8c52ee4a09d0817b90d252f158152d31d5535cd
AirborneFluff <proffitt.m.98@gmail.com>
2023-11-06 20:12:05 +0000

    Fixed current clan not updating with raffle model

--- client/src/app/core/streams/current-raffle-stream.ts
+++ client/src/app/core/streams/current-raffle-stream.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import {
   combineLatest,
   distinctUntilChanged, of,
-  switchMap
+  switchMap, take, withLatestFrom
 } from 'rxjs';
 import { RaffleIdStream } from './raffle-id-stream';
 import { ClanIdStream } from './clan-id-stream';
 import { ApiService } from '../services/api.service';
 import { Raffle } from '../../data/data-models';
 import { DataModelStream } from '../observables/data-model-stream';
+import { CurrentClanStream } from './current-clan-stream';
+import { notNullOrUndefined } from '../pipes/not-null';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrentRaffleStream extends DataModelStream<Raffle | undefined> {
-  constructor(raffleId$: RaffleIdStream, clanId$: ClanIdStream, api: ApiService) {
+  constructor(raffleId$: RaffleIdStream, clanId$: ClanIdStream, api: ApiService, private clan$: CurrentClanStream) {
     super(
       combineLatest([
         raffleId$.pipe(distinctUntilChanged()),
@@ -26,5 +28,27 @@ export class CurrentRaffleStream extends DataModelStream<Raffle | undefined> {
         })
       )
     );
+  }
+
+  override next(value: Raffle | undefined) {
+    this.clan$.pipe(
+      notNullOrUndefined(),
+      withLatestFrom(this),
+      take(1)
+    ).subscribe(([clan, currentRaffle]) => {
+      const raffleIndex = clan.raffles.findIndex(r => r.id == currentRaffle?.id);
+      if (raffleIndex == -1) return;
+
+      if (value == undefined) {
+        clan.raffles.splice(raffleIndex);
+        this.clan$.next(clan);
+        return;
+      }
+
+      clan.raffles[raffleIndex] = value;
+      this.clan$.next(clan);
+    });
+
+    super.next(value);
   }
 }
