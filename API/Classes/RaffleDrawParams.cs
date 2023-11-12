e32c778616299c6a85ebc672cd0018f7130f5065
AirborneFluff <proffitt.m.98@gmail.com>
2023-11-12 16:24:32 +0000

    Raffle params

---
+++ API/Classes/RaffleDrawParams.cs
@@ -0,0 +1,18 @@
+﻿namespace RaffleApi.Classes;
+
+public class RaffleDrawParams
+{
+    public int Delay { get; set; } = 5;
+    public bool PreventMultipleWins { get; set; } = false;
+
+    public RaffleDrawParams(int delay, bool preventMultipleWins)
+    {
+        Delay = delay;
+        PreventMultipleWins = preventMultipleWins;
+    }
+
+    public RaffleDrawParams(int delay)
+    {
+        Delay = delay;
+    }
+}
