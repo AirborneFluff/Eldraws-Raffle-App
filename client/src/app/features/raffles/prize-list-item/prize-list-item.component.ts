ecd209f06cd735982501fe153cc34ba19ca0157d
AirborneFluff <proffitt.m.98@gmail.com>
2023-11-18 15:53:04 +0000

    Simplified prize list

--- client/src/app/features/raffles/prize-list-item/prize-list-item.component.ts
+++ client/src/app/features/raffles/prize-list-item/prize-list-item.component.ts
@@ -2,10 +2,14 @@ import { Component, Input } from '@angular/core';
 import { RafflePrize } from '../../../data/models/raffle-prize';
 import { CurrentRaffleStream } from '../../../core/streams/current-raffle-stream';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
-import { map, Observable, withLatestFrom } from 'rxjs';
+import { combineLatest, map, Observable, of, startWith, switchMap, take, withLatestFrom } from 'rxjs';
 import { Observe } from '../../../core/decorators/observe';
-import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { RollWinnerFormComponent } from '../roll-winner-form/roll-winner-form.component';
+import { ConfirmDialogComponent } from '../../../shared/dialog/confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { NumericPositionPipe } from '../../../core/pipes/numeric-position';
+import { ClanIdStream } from '../../../core/streams/clan-id-stream';
+import { RaffleIdStream } from '../../../core/streams/raffle-id-stream';
+import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'prize-list-item',
@@ -14,10 +18,21 @@ import { RollWinnerFormComponent } from '../roll-winner-form/roll-winner-form.co
 })
 export class PrizeListItemComponent {
   @Input() prize!: RafflePrize;
+  @Input() showFullQuantity: boolean = false;
   @Observe("prize") public prize$!: Observable<RafflePrize>;
 
-  constructor(private raffle$: CurrentRaffleStream, private bottomSheet: MatBottomSheet) {
+  constructor(private raffle$: CurrentRaffleStream, private clanId$: ClanIdStream, private raffleId$: RaffleIdStream, private dialog: MatDialog, private numericPipe: NumericPositionPipe, private api: ApiService) {
   }
+
+  totalDonations$ = this.raffle$.pipe(
+    notNullOrUndefined(),
+    map(raffle => {
+      return raffle.entries.reduce((acc, curr) => {
+        return acc + curr.donation
+      }, 0)
+    }),
+    startWith(0)
+  )
 
   winningTicketNumber$ = this.prize$.pipe(
     map(prize => prize.winningTicketNumber)
@@ -31,11 +46,27 @@ export class PrizeListItemComponent {
     })
   )
 
-  showRollWinnerDialog() {
-    this.bottomSheet.open(RollWinnerFormComponent, {
+  removePrize() {
+    this.dialog.open(ConfirmDialogComponent, {
       data: {
-        prize: this.prize
-      },
-    });
+        title: 'Are you sure?',
+        message: `You will be removing the ${this.numericPipe.transform(this.prize.place)} prize`,
+        btnOkText: 'Yes',
+        btnCancelText: 'No',
+      }
+    }).afterClosed().pipe(
+      switchMap(confirm => {
+        if (!confirm) return of();
+
+        return combineLatest([
+          this.clanId$.pipe(notNullOrUndefined()),
+          this.raffleId$.pipe(notNullOrUndefined())
+        ]).pipe(
+          take(1),
+          switchMap(([clanId, raffleId]) => this.api.Raffles.removePrize(clanId, raffleId, this.prize.place))
+        )
+      })).subscribe(updatedRaffle => {
+        this.raffle$.next(updatedRaffle)
+    })
   }
 }
--- client/src/app/features/raffles/prize-list/prize-list.component.ts
+++ client/src/app/features/raffles/prize-list/prize-list.component.ts
@@ -1,14 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CurrentRaffleStream } from '../../../core/streams/current-raffle-stream';
-import { combineLatest, map, of, startWith, switchMap, take } from 'rxjs';
+import { map, startWith } from 'rxjs';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
-import { RafflePrize } from '../../../data/models/raffle-prize';
-import { ConfirmDialogComponent } from '../../../shared/dialog/confirm-dialog/confirm-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { NumericPositionPipe } from '../../../core/pipes/numeric-position';
-import { ClanIdStream } from '../../../core/streams/clan-id-stream';
-import { RaffleIdStream } from '../../../core/streams/raffle-id-stream';
-import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'app-prize-list',
@@ -16,10 +9,9 @@ import { ApiService } from '../../../core/services/api.service';
   styleUrls: ['./prize-list.component.scss']
 })
 export class PrizeListComponent {
-  @Input() showRollingUI: boolean = false;
   showFullQuantity: boolean = false;
 
-  constructor(public raffle$: CurrentRaffleStream, private dialog: MatDialog, private numericPipe: NumericPositionPipe, private clanId$: ClanIdStream, private raffleId$: RaffleIdStream, private api: ApiService) {
+  constructor(public raffle$: CurrentRaffleStream) {
   }
 
   prizes$ = this.raffle$.pipe(
@@ -29,38 +21,4 @@ export class PrizeListComponent {
     })),
     startWith([])
   )
-
-  totalDonations$ = this.raffle$.pipe(
-    notNullOrUndefined(),
-    map(raffle => {
-      return raffle.entries.reduce((acc, curr) => {
-        return acc + curr.donation
-      }, 0)
-    }),
-    startWith(0)
-  )
-
-  removePrize(prize: RafflePrize) {
-    this.dialog.open(ConfirmDialogComponent, {
-      data: {
-        title: 'Are you sure?',
-        message: `You will be removing the ${this.numericPipe.transform(prize.place)} prize`,
-        btnOkText: 'Yes',
-        btnCancelText: 'No',
-      }
-    }).afterClosed().pipe(
-      switchMap(confirm => {
-        if (!confirm) return of();
-
-        return combineLatest([
-          this.clanId$.pipe(notNullOrUndefined()),
-          this.raffleId$.pipe(notNullOrUndefined())
-        ]).pipe(
-          take(1),
-          switchMap(([clanId, raffleId]) => this.api.Raffles.removePrize(clanId, raffleId, prize.place))
-        )
-      })).subscribe(updatedRaffle => {
-        this.raffle$.next(updatedRaffle)
-    })
-  }
 }
