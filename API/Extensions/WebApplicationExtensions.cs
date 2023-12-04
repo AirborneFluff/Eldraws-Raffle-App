bfb81f29da8a58bee2de393c691bcf9c6e200e88
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-04 20:48:57 +0000

    Abstracted startup method

---
+++ API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RaffleApi.Data;
+using RaffleApi.Entities;
+
+namespace RaffleApi.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static async void SeedApplicationUsers(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var service = scope.ServiceProvider;
+        try
+        {
+            var userManager = service.GetRequiredService<UserManager<AppUser>>();
+            var roleManager = service.GetRequiredService<RoleManager<IdentityRole>>();
+            await UserSeed.SeedUsers(userManager, roleManager);
+        }
+        catch (Exception ex)
+        {
+            var logger = service.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred during migration");
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -91,18 +91,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var service = scope.ServiceProvider;
-try
-{
-    var userManager = service.GetRequiredService<UserManager<AppUser>>();
-    var roleManager = service.GetRequiredService<RoleManager<IdentityRole>>();
-    await UserSeed.SeedUsers(userManager, roleManager);
-}
-catch (Exception ex)
-{
-    var logger = service.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred during migration");
-}
+app.SeedApplicationUsers();
 
 app.Run();
