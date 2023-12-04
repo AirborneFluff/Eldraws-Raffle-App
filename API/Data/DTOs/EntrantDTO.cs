115d558d0cd277cec065b48b06547a9a1fb0ac98
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-04 22:34:41 +0000

    Updated DTO

--- API/Data/DTOs/EntrantDTO.cs
+++ API/Data/DTOs/EntrantDTO.cs
@@ -3,6 +3,6 @@
 public class EntrantDTO
 {
     public int Id { get; set; }
-    public int ClanId { get; set; }
     public required string Gamertag { get; set; }
+    public int TotalDonations { get; set; }
 }
