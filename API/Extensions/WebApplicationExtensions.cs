0601888e6ad490ffb2c92484455e4aa9eeecd2ec
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-04 21:47:11 +0000

    Added entrant donation aggregation method

--- API/Extensions/WebApplicationExtensions.cs
+++ API/Extensions/WebApplicationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using RaffleApi.Data;
 using RaffleApi.Entities;
 
@@ -20,6 +21,39 @@ public static class WebApplicationExtensions
         {
             var logger = service.GetRequiredService<ILogger<Program>>();
             logger.LogError(ex, "An error occurred during migration");
+        }
+    }
+
+    public static async void AggregateEntrantDonations(this WebApplication app)
+    {
+        var perform = app.Configuration.GetSection("StartupServices").GetValue<bool>("AggregateEntrantDonations");
+        if (!perform) return;
+        
+        using var scope = app.Services.CreateScope();
+        var service = scope.ServiceProvider;
+        try
+        {
+            var context = service.GetRequiredService<DataContext>();
+            var entrantDonations = await context.Entries
+                .GroupBy(entry => entry.EntrantId)
+                .Select(g => new
+                {
+                    EntantId = g.Key,
+                    TotalDonations = g.Sum(d => d.Donation)
+                }).ToListAsync();
+
+            foreach (var val in entrantDonations)
+            {
+                var entrant = await context.Entrants.SingleAsync(entrant => entrant.Id == val.EntantId);
+                entrant.TotalDonations = val.TotalDonations;
+            }
+
+            await context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            var logger = service.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred whilst aggregating entrant donations");
         }
     }
 }
