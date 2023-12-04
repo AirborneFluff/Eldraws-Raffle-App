d395bad91feb556b4e329f1c07a65ebac8a20b78
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-04 20:54:37 +0000

    Added total donations to entrants

---
+++ API/Migrations/20231204205412_AddedTotalDonations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaffleApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTotalDonations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalDonations",
+                table: "Entrants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDonations",
+                table: "Entrants");
+        }
+    }
+}
