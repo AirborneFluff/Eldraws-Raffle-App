2bda6654bb864ba7b7de4b41f30cf38263ae99c9
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-30 17:15:17 +0000

    Updated dto and included prize winners

--- API/Data/DTOs/RafflePrizeInfoDTO.cs
+++ API/Data/DTOs/RafflePrizeInfoDTO.cs
@@ -6,4 +6,5 @@ public class RafflePrizeInfoDTO
     public float DonationPercentage { get; set; }
     public string? Description { get; set; }
     public int? WinningTicketNumber { get; set; }
+    public EntrantInfoDTO? Winner { get; set; }
 }
