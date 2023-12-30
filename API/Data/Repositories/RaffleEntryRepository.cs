8df0a6877723f251a73f7f3a43ebf19a88072f38
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-30 22:07:28 +0000

    Added entry repo

---
+++ API/Data/Repositories/RaffleEntryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RaffleApi.Entities;
+using RaffleApi.Helpers;
+
+namespace RaffleApi.Data.Repositories;
+
+public sealed class RaffleEntryRepository
+{
+    private readonly DataContext _context;
+
+    public RaffleEntryRepository(DataContext _context)
+    {
+        this._context = _context;
+    }
+
+    public async Task<RaffleEntry?> GetById(int entryId)
+    {
+        return await _context.Entries.FirstOrDefaultAsync(entry => entry.Id == entryId);
+    }
+
+    public async Task<PagedList<RaffleEntry>> GetByRaffle(int raffleId, RaffleEntryParams entryParams)
+    {
+        var query = _context.Entries
+            .Include(entry => entry.Entrant)
+            .Where(entry => entry.RaffleId == raffleId)
+            .OrderByDescending(entry => entry.HighTicket);
+
+        return await PagedList<RaffleEntry>.CreateAsync(query, entryParams.PageNumber, entryParams.PageSize);
+    }
+}
