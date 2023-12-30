3c9f161d8b89f563442d86f83d919b14caa16fb8
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-30 16:35:11 +0000

    Update data model

---
+++ API/Migrations/20231230141749_AddedRaffleAggregates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaffleApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRaffleAggregates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalDonations",
+                table: "Raffles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalTickets",
+                table: "Raffles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDonations",
+                table: "Raffles");
+
+            migrationBuilder.DropColumn(
+                name: "TotalTickets",
+                table: "Raffles");
+        }
+    }
+}
