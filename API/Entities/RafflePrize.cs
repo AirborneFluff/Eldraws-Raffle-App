7beb93d0bc8c24590d47bb14a8204d0f7101037e
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-30 18:35:44 +0000

    Added entity toString methods

--- API/Entities/RafflePrize.cs
+++ API/Entities/RafflePrize.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using RaffleApi.Extensions;
+using RaffleApi.Helpers;
+
 namespace RaffleApi.Entities;
 
 public class RafflePrize
@@ -12,4 +16,18 @@ public class RafflePrize
     public float DonationPercentage { get; set; }
     public string? Description { get; set; }
     public int? WinningTicketNumber { get; set; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        
+        var position = $"**{Place.AddPositionalSynonym()}**";
+        sb.Append(position.PadString(65, 75));
+
+        if (DonationPercentage == 0) return sb.Append(Description).ToString();
+        
+        var percentage = (int)(DonationPercentage * 100);
+        sb.Append($"{percentage}% of donations");
+        return sb.ToString();
+    }
 }
